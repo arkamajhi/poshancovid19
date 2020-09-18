b9c30e9e23c92280b369fc712a709b9c018b988d
Arka Majhi <arka.majhi@gmail.com>
2020-09-19 01:49:51 +0530

    Update index.js

--- 1000days/index.js
+++ 1000days/index.js
@@ -1,12 +1,14 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    document.getElementById("login_div").style.display="initial";
-    document.getElementById("user_div").style.display="none";
-  } else {
-    document.getElementById("login_div").style.display="none";
-    document.getElementById("user_div").style.display="initial";
-  }
-});
+
+
+var user = firebase.auth().currentUser;
+
+if (user) {
+  document.getElementById("login_div").style.display="block";
+  document.getElementById("user_div").style.display="none";
+} else {
+  document.getElementById("login_div").style.display="none";
+  document.getElementById("user_div").style.display="block";
+}
 
 function login() {
   var userEmail=document.getElementById("email_field").value;
