b656291edbb1902c8a7a7657bc274a0ccec4e7f3
Arka Majhi <arka.majhi@gmail.com>
2020-11-26 12:05:45 +0530

    Create firebase.js

---
+++ 1000days/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig =
+{
+  apiKey: "AIzaSyCBKceXhch9H69RUxsoCHzwq3uia7bme2o",
+  authDomain: "test-adab6.firebaseapp.com",
+  databaseURL: "https://test-adab6.firebaseio.com",
+  projectId: "test-adab6",
+  storageBucket: "test-adab6.appspot.com",
+  messagingSenderId: "1017082139799",
+  appId: "1:1017082139799:web:8247b5e8d36d8e6f3745d2",
+  measurementId: "G-1XZM5HKBLL"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+console.log(firebase);
